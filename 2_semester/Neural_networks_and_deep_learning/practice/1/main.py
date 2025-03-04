76f2dc808521d64ca77028abb854bccb0d397956
Dina <ajjbdfnhfjkvn@gmail.com>
2025-03-04 18:22:03 +1000

    Create main.py

---
+++ 2_semester/Neural_networks_and_deep_learning/practice/1/main.py
@@ -0,0 +1,36 @@
+
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+
+digits = load_digits()
+X, y = digits.data, digits.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
+
+model = MLPClassifier(hidden_layer_sizes=(50,), max_iter=501, random_state=42) 
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_test, y_pred)
+print(f'Точность модели: {accuracy:.4f}')
+
+fig, axes = plt.subplots(2, 5, figsize=(10, 5))
+axes = axes.flatten()
+
+for i in range(10):
+    img = X_test[i].reshape(8, 8)
+    axes[i].imshow(img, cmap='gray')
+    axes[i].set_title(f"Предсказано: {y_pred[i]}\nИстинное: {y_test[i]}")
+    axes[i].axis('off')
+
+plt.tight_layout()
+plt.show()
